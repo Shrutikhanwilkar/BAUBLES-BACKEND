0e6065067d28a0bc28a8a073d3e97d4ae086fc1b
Diksha Rathod <rathoddiksha1402@gmail.com>
2025-12-16 13:32:50 +0530

    Bug Fixes

--- src/modules/admin/contactUS/contactUs.route.js
+++ src/modules/admin/contactUS/contactUs.route.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import ContactUsController from "./contactUs.controller.js";
 import authenticateToken from "../../../middleware/checkAuthToken.js";
+import { uploadArrayToFirebase } from "../../../middleware/upload.js";
 const router = Router()
 router.use(authenticateToken);
 // router.post("/create", ContactUsController.create)
 router.get("/list", ContactUsController.listContacts)
 router.get("/details/:id", ContactUsController.getContact)
 router.delete("/delete/:id", ContactUsController.deleteContact)
-router.patch("/update/:id", ContactUsController.updateContactQuery);
+router.patch(
+  "/update/:id",
+  uploadArrayToFirebase("solutionImages"),
+  ContactUsController.updateContactQuery
+);
 
 export default router;
